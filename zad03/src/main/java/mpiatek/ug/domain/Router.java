dcddc853905f5fc6537f385986ebf7e1eb64bc1b
Haruspex <mpiatek010@gmail.com>
2018-11-07 10:39:23 +0100

    Update

--- zad03/src/main/java/mpiatek/ug/domain/Router.java
+++ zad03/src/main/java/mpiatek/ug/domain/Router.java
@@ -6,7 +6,7 @@ import java.text.SimpleDateFormat;
 public class Router {
 
     private String name = "unknown";
-    private Date prod = new java.util.Date();
+    private Date prod; = new java.util.Date();
     private Boolean wireless = false;
     private Double frequency = 2.4;
 
@@ -35,20 +35,8 @@ public class Router {
     }
 
     public void setProd(String prod) {
-        // 2018-11-06  - this is bwing passed in the request
-//        this.prodDate = new java.util.Date();
-//        try {
-//            //Error Here
-//            System.out.println(prodDate);
-//            SimpleDateFormat format = new SimpleDateFormat("YYYY-MM-DD");
-//            Date formattedDate = format.parse(prodDate);
-//            this.prodDate = formattedDate;
-//        }catch (Exception e){
-//            System.out.println(e);
-//            this.prodDate = new java.util.Date();
-//        }
         try {
-            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd ");
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
             this.prod = format.parse(prod);
         }catch (Exception e){
